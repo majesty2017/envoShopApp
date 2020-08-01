e379b8ff6a4fbb85e7014926ca7df31dac62b8e4
Majesty2017 <reborndzre@gmail.com>
2020-08-01 17:21:49 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,33 +13,33 @@ export class AppComponent implements OnInit {
   public selectedIndex = 0;
   public appPages = [
     {
-      title: 'Inbox',
-      url: '/folder/Inbox',
+      title: 'Home',
+      url: '/home/Home',
       icon: 'mail'
     },
     {
       title: 'Outbox',
-      url: '/folder/Outbox',
+      url: '/home/Outbox',
       icon: 'paper-plane'
     },
     {
       title: 'Favorites',
-      url: '/folder/Favorites',
+      url: '/home/Favorites',
       icon: 'heart'
     },
     {
       title: 'Archived',
-      url: '/folder/Archived',
+      url: '/home/Archived',
       icon: 'archive'
     },
     {
       title: 'Trash',
-      url: '/folder/Trash',
+      url: '/home/Trash',
       icon: 'trash'
     },
     {
       title: 'Spam',
-      url: '/folder/Spam',
+      url: '/home/Spam',
       icon: 'warning'
     }
   ];
@@ -61,7 +61,7 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
-    const path = window.location.pathname.split('folder/')[1];
+    const path = window.location.pathname.split('home/')[1];
     if (path !== undefined) {
       this.selectedIndex = this.appPages.findIndex(page => page.title.toLowerCase() === path.toLowerCase());
     }
